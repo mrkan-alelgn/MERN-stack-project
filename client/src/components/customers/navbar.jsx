7e205c407ee8ba3c0dcd1433beab83f881c654ca
mrkan-alelgn <mrkan.alelgn1@gmail.com>
2020-06-07 16:41:58 +0000

    adding navbar

---
+++ client/src/components/customers/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is navbar</h1>
+        <button className="btn btn-secondary btn-sm">Home</button>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
