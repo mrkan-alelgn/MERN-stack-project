541d0e90e067b710599cb835e281805cb8ca582e
mrkan-alelgn <mrkan.alelgn1@gmail.com>
2020-06-16 08:55:14 +0000

    woking on sidebars and main

--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -1,15 +1,70 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import "./style.css";
+import en from "./images/en.png";
+import es from "./images/es.png";
+import de from "./images/de.png";
+import it from "./images/it.png";
+import cr from "./images/cr.png";
+import gr from "./images/gr.png";
 class Sidebar extends Component {
   render() {
     return (
-      <div className="sidenav col-sm-4 bg-info m-1 p-5">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
+      <div className="col-m-4 bg-light rounded-lg m-3" id="sticky-sidebar-left">
+        <div id="sticky-sidebar-left-inner">
+          <div class="btn btn-block btn-white disabled btn-outline-dark border-0">
+            TOP LEAGUES
+          </div>
+          <ul className="list-group ">
+            <li className="list-group-item py-1 px-0">
+              <img class="leagues" src={en}></img>England - Premier league
+            </li>
+            <li className="list-group-item py-1 px-0">
+              <img class="leagues" src={es}></img>Spain - la liga
+            </li>
+            <li className="list-group-item py-1 px-0">
+              <img class="leagues" src={de}></img>Germany - Bundesliga
+            </li>
+            <li className="list-group-item py-1 px-0">
+              <img class="leagues" src={it}></img>Italy - Serie A
+            </li>
+            <li className="list-group-item py-1 px-0">
+              <img class="leagues" src={es}></img>Spain - Segunda Division
+            </li>
+            <li className="list-group-item py-1 px-0">
+              <img class="leagues" src={en}></img>England - FA-Cup
+            </li>
+            <li className="list-group-item py-1 px-0">
+              <img class="leagues" src={de}></img>Germany - DFB-pokal
+            </li>
+            <li className="list-group-item py-1 px-0">
+              <img class="leagues" src={de}></img>Germany - 2 Bundesliga
+            </li>
+            <li className="list-group-item py- 1 px-0">
+              <img class="leagues" src={it}></img>Italy - Italy Cup
+            </li>
+            <li className="list-group-item py-1 px-0">
+              <img class="leagues" src={cr}></img>Croatia - 1 NHL
+            </li>
+            <li className="list-group-item py-1 px-0">
+              <img class="leagues" src={gr}></img>Greece - Superleague
+            </li>
+          </ul>
+        </div>
+        <div id="filterByTime">
+          <a href="#" id="filterLink">
+            Filter by Time
+          </a>
+        </div>
+        <ul className="list-group">
+          <li className="list-group-item py-1">FOOTBALL</li>
+          <li className="list-group-item py-1">ESPORTS FOOTBALL</li>
+          <li className="list-group-item py-1">BASKETBALL</li>
+          <li className="list-group-item py-1">ESPORTS BASKETBALL</li>
+          <li className="list-group-item py-1">TENNIS</li>
+          <li className="list-group-item py-1">VOLLEYBALL</li>
+          <li className="list-group-item py-1">ESPORTS VOLLEYBALL</li>
+        </ul>
       </div>
     );
   }
